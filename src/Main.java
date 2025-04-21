74cf0293eabdbaf78a3c7383e28cd9a8f0147cfa
Diegolas <diegolas.code@gmail.com>
2025-04-21 09:00:48 -0300

    panelsito

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,22 @@
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        JLabel label = new JLabel("Bro do u even code");
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(10,10,200,200);
 
-        ImageIcon image = new ImageIcon("grug.jpg");
-        label.setIcon(image);
-
-        Border border = BorderFactory.createLineBorder(Color.MAGENTA, 2);
-        label.setBorder(border);
-        // label.setBounds(20,20,350,350);
-
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.TOP);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setIconTextGap(10);
-
-        label.setForeground(new Color(0x900090));
-        label.setBackground(Color.pink);
-        label.setOpaque(true);
-        label.setFont(new Font("Jetbrains Mono", Font.PLAIN,20));
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(10,210,200,200);
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
-        // frame.setLayout(null);
+        frame.setSize(750,750);
+        frame.setLayout(null);
         frame.setVisible(true);
-        frame.add(label);
-        // frame.pack();
+        frame.add(redPanel);
+        frame.add(greenPanel);
     }
 }
